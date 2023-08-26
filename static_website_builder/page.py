a44e711bf4b064c48c554ece59847c4b5577d4a6
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-08-26 18:41:10 +0100

    Created a markdown reader utility function to remove duplication of reading markdown files

--- static_website_builder/page.py
+++ static_website_builder/page.py
@@ -1,77 +1,39 @@
 from flask import render_template
-from markupsafe import escape, Markup
-import markdown
 from static_website_builder import app
-from .utils import _page_not_found
-
+from .utils import _page_not_found, read_markdown
 
 @app.route("/page-1")
 def page_1():
-    try:
-        with open(
-            "./content/page_1.md", "r", encoding="utf-8"
-        ) as input_file:
-            text = input_file.read()
-            escaped_text = escape(text)
-            body = Markup(markdown.markdown(escaped_text))
-    except OSError as e:
-        _page_not_found(e)
+    body = read_markdown("./content/page_1.md")
+
     return render_template("page_1.html", page_body=body)
 
 
 @app.route("/page-2")
 def page_2():
-    try:
-        with open(
-            "./content/page_2.md", "r", encoding="utf-8"
-        ) as input_file:
-            text = input_file.read()
-            escaped_text = escape(text)
-            body = Markup(markdown.markdown(escaped_text))
-    except OSError as e:
-        _page_not_found(e)
+    body = read_markdown("./content/page_2.md")
+    
     return render_template("page_2.html", page_body=body)
 
 
 @app.route("/page-3")
 def page_3():
-    try:
-        with open(
-            "./content/page_3.md", "r", encoding="utf-8"
-        ) as input_file:
-            text = input_file.read()
-            escaped_text = escape(text)
-            body = Markup(markdown.markdown(escaped_text))
-    except OSError as e:
-        _page_not_found(e)
+    body = read_markdown("./content/page_3.md")
+    
     return render_template("page_3.html", page_body=body)
 
 
 @app.route("/about")
 def about_page():
-    try:
-        with open(
-            "./content/about.md", "r", encoding="utf-8"
-        ) as input_file:
-            text = input_file.read()
-            escaped_text = escape(text)
-            body = Markup(markdown.markdown(escaped_text))
-    except OSError as e:
-        _page_not_found(e)
+    body = read_markdown("./content/about.md")
+    
     return render_template("about.html", page_body=body)
 
 
 @app.route("/accessibility-statement")
 def accessibility_page():
-    try:
-        with open(
-            "./content/accessibility.md", "r", encoding="utf-8"
-        ) as input_file:
-            text = input_file.read()
-            escaped_text = escape(text)
-            body = Markup(markdown.markdown(escaped_text))
-    except OSError as e:
-        _page_not_found(e)
+    body = read_markdown("./content/accessibility.md")
+    
     return render_template("accessibility_statement.html", page_body=body)
 
 
@@ -82,15 +44,8 @@ def cookies_page():
 
 @app.route("/privacy-and-data-protection")
 def privacy_data_page():
-    try:
-        with open(
-            "./content/privacy_data.md", "r", encoding="utf-8"
-        ) as input_file:
-            text = input_file.read()
-            escaped_text = escape(text)
-            body = Markup(markdown.markdown(escaped_text))
-    except OSError as e:
-        _page_not_found(e)
+    body = read_markdown("./content/privacy_data.md")
+    
     return render_template("privacy_data.html", page_body=body)
 
 
--- static_website_builder/utils.py
+++ static_website_builder/utils.py
@@ -1,6 +1,21 @@
 from flask import abort
+from markupsafe import escape, Markup
+import markdown
 
 
 def _page_not_found(error):
     print(error)
     abort(404)
+
+def read_markdown(markdown_file):
+    try:
+        with open(
+            markdown_file, "r", encoding="utf-8"
+        ) as input_file:
+            text = input_file.read()
+            escaped_text = escape(text)
+            body = Markup(markdown.markdown(escaped_text))
+    except OSError as e:
+        _page_not_found(e)
+
+    return body
