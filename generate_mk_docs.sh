7a792631edbd8a317912df7ba450b8be75be19f7
jasonbellONS <jason.bell@ons.gov.uk>
2024-02-16 13:35:46 +0000

    Amendments to readme, generate_mk_docs.sh and website name config

--- generate_mk_docs.sh
+++ generate_mk_docs.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+website_type=$(jq -r '.content_type' ./config/website_config.json)
+if [ "$website_type" != "full_website" ] && [ "$website_type" != "mkdocs_website" ] && [ "$website_type" != "manual_website" ]; then
+  echo website_type in ./config/website_config.json it not a valid type, please amend the file and try again
+  exit 1
+fi;
+
 poetry run mkdocs build
 
 mkdir -p static_website_builder/templates/content/
